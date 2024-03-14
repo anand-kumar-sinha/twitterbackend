96e4dcec72be59dff4b9a094ca8ac68f90488387
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2024-03-14 07:15:26 +0530

    .

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -5,19 +5,20 @@ const protect = async (req, res, next) => {
   try {
     let token;
 
-    console.log(req)
+    
     if (
       req.headers.authorization &&
       req.headers.authorization.startsWith("Bearer")
     ) {
 
       try {
+        console.log('start')
         token = req.headers.authorization.split(" ")[1];
-
+        console.log(token)
         const decoded = jwt.verify(token, process.env.JWT_KEY);
-
+        console.log(decoded)
         req.user = await User.findById(decoded.id).select("-password");
-
+        console.log(req.user)
         next();
       } catch (error) {
         res.status(402).json({
